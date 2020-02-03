86015d9a8804786d9f07f4434cbae46796dd6f7d
小贱 <xiaojian2436@163.com>
2020-02-03 23:56:19 +0800

    提交文件：GitDemo1.java

---
+++ git-test-project/src/com/xiaojian/git/GitDemo1.java
@@ -0,0 +1,13 @@
+package com.xiaojian.git;
+
+public class GitDemo1 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
