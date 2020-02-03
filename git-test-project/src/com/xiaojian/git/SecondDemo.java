eb24b0672cd1e0026454177681b639ceffda873f
小贱 <xiaojian2436@163.com>
2020-02-04 00:19:42 +0800

    dev1添加了一个SecondDemo类

---
+++ git-test-project/src/com/xiaojian/git/SecondDemo.java
@@ -0,0 +1,13 @@
+package com.xiaojian.git;
+
+public class SecondDemo {
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+    private static void test1() {
+
+        System.out.println("second fun");
+    }
+}
