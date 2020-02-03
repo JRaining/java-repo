a77ef59ce788b63337c2734c2329ed15ba8c8dcf
小贱 <xiaojian2436@163.com>
2020-02-04 00:09:53 +0800

    新增成员修改fun()方法

--- git-test-project/src/com/xiaojian/git/GitDemo1.java
+++ git-test-project/src/com/xiaojian/git/GitDemo1.java
@@ -9,9 +9,13 @@ public class GitDemo1 {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        // hello test
     }
 
     public void fun(){
         System.out.println("新增了一个方法");
+        int i = 0;
+        i++;
+        System.out.println(i);
     }
 }
